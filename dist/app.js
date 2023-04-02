0af8d6d3863e66478bc94f0aba7894debff8ce48
RoxanaCamara <roxana.camara07@gmail.com>
2023-04-02 18:22:17 -0300

    feat: cambios

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sever_1 = __importDefault(require("./models/sever"));
+const dotenv_1 = __importDefault(require("dotenv"));
 console.log('Hola Nare');
+dotenv_1.default.config();
+const server = new sever_1.default();
+server.listen();
 //# sourceMappingURL=app.js.map
