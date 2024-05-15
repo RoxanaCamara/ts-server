edab4c9ca03044b96f1553fbf6ac40436115e691
RoxanaCamara <roxana.camara07@gmail.com>
2024-05-15 02:39:45 -0300

    feat: correcion table

--- db/usuario.ts
+++ db/usuario.ts
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
 import db from './connections';
 
-const Usuario = db.define('Usuario', {
+const Usuario = db.define('usuario', {
     name: { type: DataTypes.STRING },
     email: { type: DataTypes.STRING },
     state: { type: DataTypes.STRING },
